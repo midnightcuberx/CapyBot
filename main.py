446ae4f7ce6e16bda6b0e0b9b9ca47507b6afef4
Veni <63829007+midnightcuberx@users.noreply.github.com>
2023-04-09 21:23:11 +1200

    Add proper help command

--- main.py
+++ main.py
@@ -23,9 +23,135 @@ async def load_cogs():
 
 
 @bot.command()
-async def help(ctx):
-    # await ctx.send("Work in progress")
-    await ctx.send(f"List of commands:\n{', '.join([c.name for c in bot.commands])}")
+async def help(ctx, *, help_type=None):
+
+    help_list = ["misc", "shop", "balance", "earn", "gamble", "crime", "other"]
+
+    if not help_type or help_type.lower() not in help_list:
+        embed = discord.Embed(
+            title="CapyBot Help",
+            description="Capy Slay💅: type -help <category> to see info about those commands",
+            color=0xEEE657,
+        )
+        embed.add_field(name="misc", value="userinfo, ping, invite", inline=False)
+        embed.add_field(name="shop", value="shop, buy, sell, inventory", inline=False)
+        embed.add_field(
+            name="balance", value="balance, withdraw, deposit, transfer", inline=False
+        )
+        embed.add_field(
+            name="earn", value="busk, hunt, chew, fish, pm, beg, search", inline=False
+        )
+        embed.add_field(name="gamble", value="gamble, invest", inline=False)
+        embed.add_field(name="crime", value="rob, murder", inline=False)
+        embed.add_field(name="other", value="leaderboard, daily, hourly", inline=False)
+    else:
+        embed = discord.Embed(
+            title="CapyBot Help",
+            description=f"Capy Slay💅: {help_type.capitalize()} Commands",
+            color=0xEEE657,
+        )
+        if help_type.lower() == "misc":
+            embed.add_field(
+                name="``-userinfo <person>``",
+                value="gets the userinfo of a user",
+                inline=False,
+            )
+            embed.add_field(name="``-ping``", value="returns bot latency", inline=False)
+            embed.add_field(
+                name="``-invite``",
+                value="gets the invite link for the bot",
+                inline=False,
+            )
+        elif help_type.lower() == "shop":
+            embed.add_field(
+                name="``-shop``",
+                value="shows a list of shop items and their price",
+                inline=False,
+            )
+            embed.add_field(
+                name="``-buy <item> <amount (optional)>``",
+                value="buy an item from the shop",
+                inline=False,
+            )
+            embed.add_field(
+                name="``-sell <item> <amount (optional)>``",
+                value="sell an item from the shop",
+                inline=False,
+            )
+        elif help_type.lower() == "balance":
+            embed.add_field(
+                name="``-balance <user (optional)>``",
+                value="shows the balance of a user",
+                inline=False,
+            )
+            embed.add_field(
+                name="``-withdraw <amount>``",
+                value="withdraw money from your bank",
+                inline=False,
+            )
+            embed.add_field(
+                name="``-deposit <amount>``",
+                value="deposit money into your bank",
+                inline=False,
+            )
+            embed.add_field(
+                name="``-transfer/give <user> <amount>``",
+                value="gives money to a user",
+                inline=False,
+            )
+        elif help_type.lower() == "earn":
+            embed.add_field(
+                name="``-busk``", value="earn money by busking!", inline=False
+            )
+            embed.add_field(
+                name="``-hunt``", value="earn money by hunting!", inline=False
+            )
+            embed.add_field(
+                name="``-chew``", value="earn money by chewing!", inline=False
+            )
+            embed.add_field(
+                name="``-fish``", value="earn money by fishing!", inline=False
+            )
+            embed.add_field(
+                name="``-pm``", value="earn money by posting memes!", inline=False
+            )
+            embed.add_field(name="``-beg``", value="earn money by begging!")
+            embed.add_field(
+                name="``-search``",
+                value="earn money by searching random places!",
+                inline=False,
+            )
+        elif help_type.lower() == "gamble":
+            embed.add_field(
+                name="``-invest <amount>``",
+                value="try your luck investing money!",
+                inline=False,
+            )
+            embed.add_field(
+                name="``-gamble <amount>``",
+                value="try your hand at gambling!",
+                inline=False,
+            )
+        elif help_type.lower() == "crime":
+            embed.add_field(name="``-rob <user>``", value="rob someone", inline=False)
+            embed.add_field(
+                name="``-murder <user>``", value="murder someone", inline=False
+            )
+        elif help_type.lower() == "other":
+            embed.add_field(
+                name="``-leaderboard (lb) <item>``",
+                value="returns leaderboard for an item",
+                inline=False,
+            )
+            embed.add_field(
+                name="``-daily``", value="get your daily coins!", inline=False
+            )
+            embed.add_field(
+                name="``-hourly``", value="get your hourly coins!", inline=False
+            )
+
+    await ctx.send(embed=embed)
+
 
 
 @bot.command()
