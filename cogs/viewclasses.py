c8a42e946d415b2556d778fc5087e60e31bd4f49
Veni <63829007+midnightcuberx@users.noreply.github.com>
2023-04-17 22:05:37 +1200

    Update damage

--- cogs/viewclasses.py
+++ cogs/viewclasses.py
@@ -3,8 +3,9 @@ import random
 
 
 class FightView(discord.ui.View):
-    def __init__(self, author, timeout):
+    def __init__(self, author, health, timeout):
         self.author = author
+        self.health = health
         self.button_pressed = False
         self.damage = 0
         super().__init__(timeout=timeout)
@@ -23,18 +24,28 @@ class FightView(discord.ui.View):
     async def interaction_check(self, interaction: discord.Interaction):
         return interaction.user.id == self.author.id
 
-    # creates a button when pressed you send message
-    @discord.ui.button(label="Hit", style=discord.ButtonStyle.primary)
-    async def hit(self, interaction: discord.Interaction, button: discord.ui.Button):
+    def get_damage(self):
         chance = random.randint(1, 100)
         if chance == 1:
             self.damage = 100
-        elif chance > 1 and chance < 7:
-            self.damage = random.randint(40, 70)
-        elif chance > 90:
-            self.damage = random.randint(30, 50)
+        elif chance > 1 and chance < 5:
+            t = random.randint(70, 90)
+            self.damage = 15 + round(self.health * t / 100)
+            if self.damage > 99:
+                self.damage = 99
+        elif chance > 75:
+            t = random.randint(30, 50)
+            self.damage = 10 + round(self.health * t / 100)
         else:
-            self.damage = random.randint(1, 20)
+            b = random.randint(0, 5)
+            t = random.randint(0, 30)
+            self.damage = b + round(self.health * t / 100)
+
+    # creates a button when pressed you send message
+    @discord.ui.button(label="Hit", style=discord.ButtonStyle.primary)
+    async def hit(self, interaction: discord.Interaction, button: discord.ui.Button):
+
+        self.get_damage()
 
         await interaction.response.send_message(
             f"You dealt {self.damage} damage to your opponent!", ephemeral=True
